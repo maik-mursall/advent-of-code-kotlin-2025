bf3b3a605271f6a39a99a8f3636600b89aa7c63b
Maik Mursall <maik+github@mursall.dev>
2025-12-02 00:06:19 +0100

    Improved Day 1 result

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,10 +1,36 @@
+data class SafeSimulationResult(
+    val zeroCount: Int,
+    val wrapCount: Int
+) {
+    val totalCount = zeroCount + wrapCount
+}
+
 fun main() {
-    fun part1(input: List<String>): Int {
+    fun simulatePasswordCombination(
+        input: List<String>
+    ): SafeSimulationResult {
         var dialPosition = 50
-        var hitCount = 0
+        var zeroCount = 0
+        var wrapCount = 0
+
+        fun evaluateStepResult(newDialPosition: Int, wrapped: Boolean) {
+            // If dialPosition was 0 before, we don't count any change as a wrap.
+            if (wrapped && dialPosition != 0) {
+                if (newDialPosition == 0) {
+                    zeroCount++
+                } else {
+                    wrapCount++
+                }
+            } else if (newDialPosition == 0) {
+                zeroCount++
+            }
+        }
+
         input.forEach { inputLine ->
             val turnOperator = inputLine.first()
             val turnCount = inputLine.slice(1 until inputLine.length).toInt()
+
+            wrapCount += turnCount / 100
 
             val delta = when (turnOperator) {
                 'R' -> turnCount % 100
@@ -16,63 +42,33 @@ fun main() {
 
             if (unclampedNewDialPosition >= 100) {
                 // unclampedNewDialPosition is positive
-                dialPosition = unclampedNewDialPosition % 100
+                val newDialPosition = unclampedNewDialPosition % 100
+                evaluateStepResult(newDialPosition, true)
+                dialPosition = newDialPosition
             } else if (unclampedNewDialPosition < 0) {
                 // unclampedNewDialPosition is negative
-                dialPosition = 100 + unclampedNewDialPosition
+                val newDialPosition = 100 + unclampedNewDialPosition
+                evaluateStepResult(newDialPosition, true)
+                dialPosition = newDialPosition
             } else {
                 // unclampedNewDialPosition = 0
+                evaluateStepResult(unclampedNewDialPosition, false)
                 dialPosition = unclampedNewDialPosition
-            }
-
-            if (dialPosition == 0) {
-                hitCount++
             }
         }
 
-        return hitCount
+        return SafeSimulationResult(
+            zeroCount = zeroCount,
+            wrapCount = wrapCount
+        )
+    }
+
+    fun part1(input: List<String>): Int {
+        return simulatePasswordCombination(input).zeroCount
     }
 
     fun part2(input: List<String>): Int {
-        var dialPosition = 50
-        var hitCount = 0
-        input.forEach { inputLine ->
-            val turnOperator = inputLine.first()
-            val turnCount = inputLine.slice(1 until inputLine.length).toInt()
-
-            val wrapCount = turnCount / 100
-            hitCount += wrapCount
-
-            val delta = when (turnOperator) {
-                'R' -> turnCount % 100
-                'L' -> -(turnCount % 100)
-                else -> throw IllegalStateException("Unknown in $inputLine")
-            }
-
-            val unclampedNewDialPosition = dialPosition + delta
-
-            if (unclampedNewDialPosition >= 100) {
-                // unclampedNewDialPosition is positive
-                if (dialPosition != 0) {
-                    hitCount++
-                }
-                dialPosition = unclampedNewDialPosition % 100
-            } else if (unclampedNewDialPosition < 0) {
-                // unclampedNewDialPosition is negative
-                if (dialPosition != 0) {
-                    hitCount++
-                }
-                dialPosition = 100 + unclampedNewDialPosition
-            } else {
-                // unclampedNewDialPosition = 0
-                if (unclampedNewDialPosition == 0) {
-                    hitCount++
-                }
-                dialPosition = unclampedNewDialPosition
-            }
-        }
-
-        return hitCount
+        return simulatePasswordCombination(input).totalCount
     }
 
     // Test if implementation meets criteria from the description, like:
