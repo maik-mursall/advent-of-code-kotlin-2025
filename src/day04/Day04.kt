73ee8b8e192ca9e5046bddcb0d1a13592782a61f
Maik Mursall <maik+github@mursall.dev>
2025-12-03 22:55:59 +0100

    Day04 preparations

---
+++ src/day04/Day04.kt
@@ -0,0 +1,25 @@
+package day04
+
+import checkEquals
+import println
+import readInput
+
+fun main() {
+    fun part1(input: List<String>) = 0L
+
+    fun part2(input: List<String>) = 0L
+
+    // Test if implementation meets criteria from the description, like:
+    checkEquals(part1(listOf()), 0L)
+    checkEquals(part2(listOf()), 0L)
+
+    // Or read a large test input from the `src/Day04_test.txt` file:
+    val testInput = readInput("day04/Day04_test")
+    checkEquals(part1(testInput), 0L)
+    checkEquals(part2(testInput), 0L)
+
+    // Read the input from the `src/Day04.txt` file.
+    val input = readInput("day04/Day04")
+    part1(input).println("Part 1: ")
+    part2(input).println("Part 2: ")
+}
