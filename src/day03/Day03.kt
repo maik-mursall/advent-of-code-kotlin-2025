938cd1407f6251e53858c09723b251c5b94028e3
Maik Mursall <maik+github@mursall.dev>
2025-12-03 16:29:55 +0100

    Day03 part1

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -2,11 +2,16 @@ package day03
 
 import checkEquals
 import println
-import readInputCsv
+import readInput
 
 fun main() {
-    fun part1(input: List<String>): Long {
-        return 0L
+    fun part1(input: List<String>) = input.sumOf { batteryBank ->
+        (0 until (batteryBank.length - 1)).maxOf { i ->
+            val currentCandidate = batteryBank[i]
+            batteryBank.substring(i + 1, batteryBank.length).maxOf { nextCandidate ->
+                "$currentCandidate$nextCandidate".toLong()
+            }
+        }
     }
 
     fun part2(input: List<String>): Long {
@@ -14,16 +19,16 @@ fun main() {
     }
 
     // Test if implementation meets criteria from the description, like:
-    checkEquals(part1(listOf()), 0L)
-    checkEquals(part2(listOf()), 0L)
+    checkEquals(part1(listOf("12345")), 45L)
+    //checkEquals(part2(listOf("987654321111111")), 987654321111L)
 
     // Or read a large test input from the `src/Day03_test.txt` file:
-    val testInput = readInputCsv("day03/Day03_test")
-    checkEquals(part1(testInput), 0L)
-    checkEquals(part2(testInput), 0L)
+    val testInput = readInput("day03/Day03_test")
+    checkEquals(part1(testInput), 357L)
+    //checkEquals(part2(testInput), 3121910778619L)
 
     // Read the input from the `src/Day03.txt` file.
-    val input = readInputCsv("day03/Day03")
+    val input = readInput("day03/Day03")
     part1(input).println("Part 1: ")
     part2(input).println("Part 2: ")
 }
