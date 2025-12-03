38e29b194817804b81a818638561105c0103b4ab
Maik Mursall <maik+github@mursall.dev>
2025-12-03 22:54:20 +0100

    Day03 part2

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -14,18 +14,35 @@ fun main() {
         }
     }
 
-    fun part2(input: List<String>): Long {
-        return 0L
+    fun part2(input: List<String>) = input.sumOf { batteryBank ->
+        val usedBatteries = mutableListOf<Char>()
+
+        var currentIndex = 0
+        while (usedBatteries.size < 12) {
+            val maxIndex = batteryBank.length - (12 - usedBatteries.size)
+            if (currentIndex > maxIndex) {
+                usedBatteries.addAll(batteryBank.slice(currentIndex until batteryBank.lastIndex).toList())
+                break
+            }
+
+            val maxBatteryIndex = (currentIndex..maxIndex).maxBy { batteryBank[it] }
+            usedBatteries.add(batteryBank[maxBatteryIndex])
+            currentIndex = maxBatteryIndex + 1
+        }
+
+        usedBatteries.joinToString("").toLong()
     }
 
     // Test if implementation meets criteria from the description, like:
     checkEquals(part1(listOf("12345")), 45L)
-    //checkEquals(part2(listOf("987654321111111")), 987654321111L)
+    checkEquals(part2(listOf("987654321111111")), 987654321111L)
+    checkEquals(part2(listOf("191876543211119")), 987654321119L)
+    checkEquals(part2(listOf("818181911112111")), 888911112111L)
 
     // Or read a large test input from the `src/Day03_test.txt` file:
     val testInput = readInput("day03/Day03_test")
     checkEquals(part1(testInput), 357L)
-    //checkEquals(part2(testInput), 3121910778619L)
+    checkEquals(part2(testInput), 3121910778619L)
 
     // Read the input from the `src/Day03.txt` file.
     val input = readInput("day03/Day03")
