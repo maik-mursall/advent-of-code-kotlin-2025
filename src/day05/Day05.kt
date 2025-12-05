0745fdad3a21dcdbcedce74374c3df003fe275f0
Maik Mursall <maik+github@mursall.dev>
2025-12-05 13:31:11 +0100

    Day05 part1

--- src/day05/Day05.kt
+++ src/day05/Day05.kt
@@ -4,8 +4,29 @@ import println
 import readInput
 
 fun main() {
-    fun part1(input: List<String>): Long {
-        return 0L
+    fun extractRanges(input: List<String>): Pair<List<LongRange>, List<Long>> {
+        val (validIndices, indicesToCheckStrings) = input
+            .filter { it.isNotEmpty() }
+            .partition { it.contains('-') }
+
+        val validIndicesRange = validIndices.map {
+            val (start, end) = it.split('-')
+            start.toLong()..end.toLong()
+        }
+
+        val indicesToCheck = indicesToCheckStrings.map(String::toLong)
+
+        return validIndicesRange to indicesToCheck
+    }
+
+    fun part1(input: List<String>): Int {
+        val (validIndicesRanges, indicesToCheck) = extractRanges(input)
+
+        return indicesToCheck.count { indexToCheck ->
+            validIndicesRanges.any {
+                it.contains(indexToCheck)
+            }
+        }
     }
 
     fun part2(input: List<String>): Long {
@@ -13,16 +34,16 @@ fun main() {
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf()) == 0L)
-    check(part2(listOf()) == 0L)
+    //check(part1(listOf()) == 0L)
+    //check(part2(listOf()) == 0L)
 
     // Or read a large test input from the `src/Day05_test.txt` file:
     val testInput = readInput("day05/Day05_test")
-    check(part1(testInput) == 0L)
-    check(part2(testInput) == 0L)
+    check(part1(testInput) == 3)
+    //check(part2(testInput) == 0L)
 
     // Read the input from the `src/Day05.txt` file.
     val input = readInput("day05/Day05")
     part1(input).println()
-    part2(input).println()
+    //part2(input).println()
 }
