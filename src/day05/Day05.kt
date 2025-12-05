cf0358ac3881aef4dbc3b4987d57333ad9f40441
Maik Mursall <maik+github@mursall.dev>
2025-12-05 16:41:12 +0100

    Day05 part2

--- src/day05/Day05.kt
+++ src/day05/Day05.kt
@@ -1,5 +1,6 @@
 package day05
 
+import checkEquals
 import println
 import readInput
 
@@ -29,21 +30,71 @@ fun main() {
         }
     }
 
+    fun LongRange.fastSize(): Long {
+        return (this.last + 1) - this.first
+    }
+
+    fun mergeRangesStep(ranges: List<LongRange>): List<LongRange> = ranges.fold(mutableListOf()) { acc, range ->
+        val firstHit = acc.indexOfFirst {
+            val rangeStartInside = range.first in it
+            val rangeEndInside = range.last in it
+
+            if (rangeStartInside && rangeEndInside) { return@fold acc } // range is fully inside existing range
+
+            if (!rangeStartInside && !rangeEndInside && (range.first > (it.last + 1) || range.last < (it.first - 1))) {
+                return@indexOfFirst false
+            }
+
+            return@indexOfFirst true
+        }
+
+        if (firstHit == -1) {
+            acc.add(range)
+        } else {
+            val hitRange = acc[firstHit]
+            val newRange = minOf(hitRange.first, range.first)..maxOf(hitRange.last, range.last)
+            acc[firstHit] = newRange
+        }
+
+        acc
+    }
+
+    fun mergeRanges(ranges: List<LongRange>): List<LongRange> {
+        var currentRanges = ranges
+        while (true) {
+            val merged = mergeRangesStep(currentRanges)
+            if (merged.hashCode() == currentRanges.hashCode()) {
+                return merged
+            }
+            currentRanges = merged
+        }
+    }
+
     fun part2(input: List<String>): Long {
-        return 0L
+        val (validIndicesRanges) = extractRanges(input)
+
+        return mergeRanges(validIndicesRanges).fold(0L) { count, range ->
+            count + range.fastSize()
+        }
     }
 
     // Test if implementation meets criteria from the description, like:
     //check(part1(listOf()) == 0L)
-    //check(part2(listOf()) == 0L)
+    checkEquals(part2(listOf(
+        "3-5",
+        "10-14",
+        "16-20",
+        "12-18",
+        "19-20"
+    )), 14L)
 
     // Or read a large test input from the `src/Day05_test.txt` file:
     val testInput = readInput("day05/Day05_test")
-    check(part1(testInput) == 3)
-    //check(part2(testInput) == 0L)
+    checkEquals(part1(testInput), 3)
+    checkEquals(part2(testInput), 14L)
 
     // Read the input from the `src/Day05.txt` file.
     val input = readInput("day05/Day05")
-    part1(input).println()
-    //part2(input).println()
+    part1(input).println("Part 1: ")
+    part2(input).println("Part 2: ")
 }
