1bae424e47cbaf450fac591c8c1dd7d10c92b0f0
Maik Mursall <maik+github@mursall.dev>
2025-12-07 22:17:59 +0100

    Day07: part2

--- src/day07/Day07.kt
+++ src/day07/Day07.kt
@@ -36,8 +36,31 @@ fun main() {
         return splitterHitCount
     }
 
-    fun part2(input: List<String>): Long {
-        return 0L
+    fun part2(state: List<String>): Long {
+        val memo = hashMapOf<Pair<Int, Int>, Long>()
+
+        fun part2Step(x: Int, y: Int): Long {
+            if (y == state.size - 1) {
+                return 1L
+            }
+
+            if (state[y][x] == '^') {
+                if (memo.containsKey(Pair(x, y))) {
+                    return memo[Pair(x, y)]!!
+                }
+
+                val firstStepResult = part2Step(x - 1, y)
+                val secondStepResult = part2Step(x + 1, y)
+                val result = firstStepResult + secondStepResult
+                memo[Pair(x, y)] = result
+                return result
+            }
+
+            return part2Step(x, y + 1)
+        }
+
+        val startX = state.first().indexOf('S')
+        return part2Step(startX, 1)
     }
 
     val smallTestInput = """
@@ -50,12 +73,12 @@ fun main() {
     """.trimIndent().lines()
     // Test if implementation meets criteria from the description, like:
     checkEquals(part1(smallTestInput), 3L)
-    checkEquals(part2(smallTestInput), 0L)
+    checkEquals(part2(smallTestInput), 4L)
 
     // Or read a large test input from the `src/day07/Day07_test.txt` file:
     val testInput = readInput("day07/Day07_test")
     checkEquals(part1(testInput), 21L)
-    checkEquals(part2(testInput), 0L)
+    checkEquals(part2(testInput), 40L)
 
     // Read the input from the `src/day07/Day07.txt` file.
     val input = readInput("day07/Day07")
