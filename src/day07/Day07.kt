179db1267a69e1cc6720dc574a3c8ecab58b2b27
Maik Mursall <maik+github@mursall.dev>
2025-12-07 17:57:40 +0100

    Day07: preparations

---
+++ src/day07/Day07.kt
@@ -0,0 +1,29 @@
+package day07
+
+import checkEquals
+import println
+import readInput
+
+fun main() {
+    fun part1(input: List<String>): Long {
+        return 0L
+    }
+
+    fun part2(input: List<String>): Long {
+        return 0L
+    }
+
+    // Test if implementation meets criteria from the description, like:
+    checkEquals(part1(listOf()), 0L)
+    checkEquals(part2(listOf()), 0L)
+
+    // Or read a large test input from the `src/day07/Day07_test.txt` file:
+    val testInput = readInput("day07/Day07_test")
+    checkEquals(part1(testInput), 0L)
+    checkEquals(part2(testInput), 0L)
+
+    // Read the input from the `src/day07/Day07.txt` file.
+    val input = readInput("day07/Day07")
+    part1(input).println("Part 1: ")
+    part2(input).println("Part 2: ")
+}
