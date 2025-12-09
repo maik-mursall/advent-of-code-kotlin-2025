701dd86854b0f09672ac1191c39be8f7ad44ad8e
Maik Mursall <maik+github@mursall.dev>
2025-12-10 00:51:01 +0100

    Day08: part2 + cleanup

--- src/day08/Day08.kt
+++ src/day08/Day08.kt
@@ -37,59 +37,79 @@ data class Link(
 )
 
 fun main() {
-    fun part1(input: List<String>, maxIterations: Int): Long {
-        val junctionBoxLocations = input.map(Vector3D::fromString)
-        val possibleEdges = junctionBoxLocations.flatMapIndexed { index, junctionBoxToCheck ->
+    fun calculatePossibleLinks(junctionBoxLocations: List<Vector3D>) =
+        junctionBoxLocations.flatMapIndexed { index, junctionBoxToCheck ->
             junctionBoxLocations
                 .drop(index + 1)
                 .map { Link(junctionBoxToCheck, it, junctionBoxToCheck.distanceTo(it)) }
         }.sortedBy { it.distance }
 
-        val circuits = mutableListOf<Set<Vector3D>>()
-        possibleEdges.take(maxIterations).forEach { elementsWithSmallestDistance ->
-            val fromCircuit = circuits.indexOfFirst { it.contains(elementsWithSmallestDistance.from) }
-            val toCircuit = circuits.indexOfFirst { it.contains(elementsWithSmallestDistance.to) }
+    fun MutableList<Set<Vector3D>>.performUnion(link: Link) {
+        val fromCircuit = this.indexOfFirst { it.contains(link.from) }
+        val toCircuit = this.indexOfFirst { it.contains(link.to) }
 
-            if (fromCircuit == -1 && toCircuit == -1) {
-                circuits.add(setOf(elementsWithSmallestDistance.from, elementsWithSmallestDistance.to))
-            } else if (fromCircuit == toCircuit) {
-                // no-op
-            } else if (fromCircuit == -1) {
-                circuits[toCircuit] += elementsWithSmallestDistance.from
-            } else if (toCircuit == -1) {
-                circuits[fromCircuit] += elementsWithSmallestDistance.to
-            } else {
-                circuits[fromCircuit] = circuits[fromCircuit] union circuits[toCircuit]
-                circuits.removeAt(toCircuit)
-            }
+        if (fromCircuit == -1 && toCircuit == -1) {
+            this.add(setOf(link.from, link.to))
+        } else if (fromCircuit == toCircuit) {
+            // no-op
+        } else if (fromCircuit == -1) {
+            this[toCircuit] += link.from
+        } else if (toCircuit == -1) {
+            this[fromCircuit] += link.to
+        } else {
+            this[fromCircuit] = this[fromCircuit] union this[toCircuit]
+            this.removeAt(toCircuit)
         }
+    }
+
+    fun solveForCircuits(
+        links: List<Link>
+    ): List<Set<Vector3D>> {
+        return links.fold(mutableListOf()) { circuits, link ->
+            circuits.performUnion(link)
+            circuits
+        }
+    }
+
+    fun part1(input: List<String>, maxIterations: Int): Long {
+        val junctionBoxLocations = input.map(Vector3D::fromString)
+        val possibleEdges = calculatePossibleLinks(junctionBoxLocations)
+        val circuits = solveForCircuits(possibleEdges.take(maxIterations))
 
         return circuits.sortedByDescending { it.size }.take(3).fold(1L) { acc, ds -> acc * ds.size }
     }
 
-    fun part2(input: List<String>): Long {
-        return 0L
+    fun solveForSingleCircuit(
+        links: List<Link>,
+        numberOfJunctions: Int,
+    ): Link? {
+        val circuits = mutableListOf<Set<Vector3D>>()
+        links.forEach { link ->
+            circuits.performUnion(link)
+
+            if (circuits.singleOrNull()?.size == numberOfJunctions) {
+                return link
+            }
+        }
+        return null
     }
 
-    // Test if implementation meets criteria from the description, like:
-    val minimalTestData = listOf(
-        "162,817,812",
-        "57,618,57",
-        "906,360,560",
-        "592,479,940",
-    )
-    //checkEquals(part1(minimalTestData), 0L)
-    //checkEquals(part2(minimalTestData), 0L)
+    fun part2(input: List<String>): Long {
+        val junctionBoxLocations = input.map(Vector3D::fromString)
+        val finalLink = solveForSingleCircuit(calculatePossibleLinks(junctionBoxLocations), input.size)!!
+
+        return finalLink.from.x * finalLink.to.x
+    }
 
     // Or read a large test input from the `src/day08/Day08_test.txt` file:
     val testInput = readInput("day08/Day08_test")
     checkEquals(part1(testInput, 10), 40L)
-    checkEquals(part2(testInput), 0L)
+    checkEquals(part2(testInput), 25272L)
 
     println("All tests passed. ===============================")
 
     // Read the input from the `src/day08/Day08.txt` file.
     val input = readInput("day08/Day08")
-    part1(input, 1000).println("Part 1: ") // 27825, 15860 => wrong; 131150 => correct
+    part1(input, 1000).println("Part 1: ")
     part2(input).println("Part 2: ")
 }
