56eaa3dd3a3e233ceb3988d199f8951f5fb7303f
Maik Mursall <maik+github@mursall.dev>
2025-12-17 00:06:44 +0100

    Day11: part2 + part1 improvements

--- src/day11/Day11.kt
+++ src/day11/Day11.kt
@@ -7,41 +7,76 @@ import readInput
 typealias Node = String
 
 fun main() {
-    fun parseInput(input: List<String>): Map<Node, List<Node>> = input.map {
+    fun parseInput(input: List<String>): Map<Node, List<Node>> = input.associate {
         val parts = it.split(' ')
         val node = parts[0].dropLast(1)
 
         node to parts.drop(1)
-    }.toMap()
+    }
 
-    fun part1(input: List<String>): Long {
-        val nodesWithConnections = parseInput(input)
+    fun findPathCount(
+        nodesWithConnections: Map<Node, List<Node>>,
+        from: Node,
+        to: Node
+    ): Long {
+        val memory = mutableMapOf<Node, Long>()
 
         fun step(
             connections: List<Node>
         ): Long = connections.fold(0L) { acc, node ->
-            if (node == "out") {
+            if (node == to) {
                 acc + 1L
             } else {
-                acc + step(nodesWithConnections[node]!!)
+                if (memory.containsKey(node)) {
+                    acc + memory[node]!!
+                } else {
+                    val nextNodes = nodesWithConnections[node]
+                    if (nextNodes != null) {
+                        val stepResult = step(nextNodes)
+
+                        memory[node] = stepResult
+
+                        acc + stepResult
+                    } else {
+                        acc
+                    }
+                }
             }
         }
 
-        return step(nodesWithConnections["you"]!!)
+        return step(nodesWithConnections[from]!!)
+    }
+
+    fun part1(input: List<String>): Long {
+        val nodesWithConnections = parseInput(input)
+
+        return findPathCount(nodesWithConnections, "you", "out")
     }
 
     fun part2(input: List<String>): Long {
-        return 0L
+        val nodesWithConnections = parseInput(input)
+
+        val pathCountSvrDac = findPathCount(nodesWithConnections, "svr", "dac")
+        val pathCountSvrFft = findPathCount(nodesWithConnections, "svr", "fft")
+
+        if (pathCountSvrDac < pathCountSvrFft) {
+            val pathCountDacFft = findPathCount(nodesWithConnections, "dac", "fft")
+            val pathCountFftOut = findPathCount(nodesWithConnections, "fft", "out")
+            return pathCountSvrDac * pathCountDacFft * pathCountFftOut
+        } else {
+            val pathCountFftDac = findPathCount(nodesWithConnections, "fft", "dac")
+            val pathCountDacOut = findPathCount(nodesWithConnections, "dac", "out")
+            return pathCountSvrFft * pathCountFftDac * pathCountDacOut
+        }
     }
 
     // Test if implementation meets criteria from the description, like:
     //checkEquals(part1(listOf()), 0L)
-    checkEquals(part2(listOf()), 0L)
+    //checkEquals(part2(listOf()), 0L)
 
     // Or read a large test input from the `src/day11/Day11_test.txt` file:
-    val testInput = readInput("day11/Day11_test")
-    checkEquals(part1(testInput), 5L)
-    checkEquals(part2(testInput), 0L)
+    checkEquals(part1(readInput("day11/Day11_test")), 5L)
+    checkEquals(part2(readInput("day11/Day11_test2")), 2L)
 
     // Read the input from the `src/day11/Day11.txt` file.
     val input = readInput("day11/Day11")
