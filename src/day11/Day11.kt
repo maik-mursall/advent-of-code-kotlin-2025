d8ff29450292fff0d525fe17d83d7d81b6b4cca2
Maik Mursall <maik+github@mursall.dev>
2025-12-16 21:58:34 +0100

    Day11: part1

--- src/day11/Day11.kt
+++ src/day11/Day11.kt
@@ -4,9 +4,28 @@ import checkEquals
 import println
 import readInput
 
+typealias Node = String
+
 fun main() {
     fun part1(input: List<String>): Long {
-        return 0L
+        val nodesWithConnections: Map<Node, List<Node>> = input.map {
+            val parts = it.split(' ')
+            val node = parts[0].dropLast(1)
+
+            node to parts.drop(1)
+        }.toMap()
+
+        fun step(
+            connections: List<Node>
+        ): Long = connections.fold(0L) { acc, node ->
+            if (node == "out") {
+                acc + 1L
+            } else {
+                acc + step(nodesWithConnections[node]!!)
+            }
+        }
+
+        return step(nodesWithConnections["you"]!!)
     }
 
     fun part2(input: List<String>): Long {
@@ -14,12 +33,12 @@ fun main() {
     }
 
     // Test if implementation meets criteria from the description, like:
-    checkEquals(part1(listOf()), 0L)
+    //checkEquals(part1(listOf()), 0L)
     checkEquals(part2(listOf()), 0L)
 
     // Or read a large test input from the `src/day11/Day11_test.txt` file:
     val testInput = readInput("day11/Day11_test")
-    checkEquals(part1(testInput), 0L)
+    checkEquals(part1(testInput), 5L)
     checkEquals(part2(testInput), 0L)
 
     // Read the input from the `src/day11/Day11.txt` file.
